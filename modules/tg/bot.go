df000f040432c2dc1e54954a67ecbe0f4517931e
Ануфриев Георгий <far.galaxy.klg@gmail.com>
2023-10-01 10:41:24 +0400

    Добавлено: немного текста Удалено: сохранение чата в БД

--- modules/tg/bot.go
+++ modules/tg/bot.go
@@ -189,22 +189,28 @@ func (bot *Bot) ChatActions(update tgbotapi.Update) (tgbotapi.Message, error) {
 		action.OldChatMember.Status != "administrator" {
 		msg := tgbotapi.NewMessage(
 			action.Chat.ID,
-			"Привет! Ты меня добавил в чат, и теперь можно смотерть расписание прямо отсюда :)",
+			"Всем привет! Теперь вы можете посмотреть расписание прямо в чате :)\n"+
+				fmt.Sprintf("Просто начни писать @%s, а далее введи и выбери нужную группу или преподавателя\n\n", bot.Name)+
+				"Подробнее: https://youtube.com/shorts/oCPIsoOILYU",
 		)
-		group := database.GroupChatInfo{
-			ChatID: action.Chat.ID,
-		}
-		if _, err := bot.DB.InsertOne(group); err != nil {
-			return nilMsg, err
-		}
+		/*
+			group := database.GroupChatInfo{
+				ChatID: action.Chat.ID,
+			}
+			if _, err := bot.DB.InsertOne(group); err != nil {
+				return nilMsg, err
+			}
+		*/
 
 		return bot.TG.Send(msg)
 	} else if action.NewChatMember.Status == "administrator" {
 		msg := tgbotapi.NewMessage(
 			action.Chat.ID,
 			"Не думаю, что есть необходимость делать меня администратором\n"+
-				"А то вдруг начнётся восстание машин и я случайно поудаляю ваши чаты (:",
+				"А то вдруг начнётся восстание машин и я случайно поудаляю ваши чаты (:\n\n"+
+				"Отменить действие можно в меню <b>Управление группой -> Администраторы</b>",
 		)
+		msg.ParseMode = tgbotapi.ModeHTML
 
 		return bot.TG.Send(msg)
 	}
@@ -384,13 +390,15 @@ func (bot *Bot) GetSheduleFromCmd(
 }
 
 func (bot *Bot) HandleGroup(msg *tgbotapi.Message, now time.Time) (tgbotapi.Message, error) {
-	group := database.GroupChatInfo{
-		ChatID: msg.Chat.ID,
-	}
-	if _, err := bot.DB.Get(&group); err != nil {
-		return nilMsg, err
-	}
-	bot.Debug.Printf("ChatCommand [%d] %s", group.ChatID, msg.Text)
+	/*
+		group := database.GroupChatInfo{
+			ChatID: msg.Chat.ID,
+		}
+		if _, err := bot.DB.Get(&group); err != nil {
+			return nilMsg, err
+		}
+	*/
+	bot.Debug.Printf("ChatCommand [%d] %s", msg.Chat.ID, msg.Text)
 
 	// TODO: добавить игнор команд для групповых чатов в ЛС
 	/*
@@ -414,13 +422,13 @@ func (bot *Bot) HandleGroup(msg *tgbotapi.Message, now time.Time) (tgbotapi.Mess
 		}*/
 	if KeywordContains(msg.Text, []string{"/group", "/staff"}) {
 		fakeUser := database.TgUser{
-			TgId:   group.ChatID,
+			TgId:   msg.Chat.ID,
 			PosTag: database.Ready,
 		}
 
 		return bot.GetSheduleFromCmd(now, &fakeUser, msg.Text)
 	}
-	ans := tgbotapi.NewMessage(msg.Chat.ID, "Исполняю команду")
+	ans := tgbotapi.NewMessage(msg.Chat.ID, "Исполняю команду, которую не знаю, как исполнять")
 	ans.ReplyMarkup = CancelKey()
 
 	return bot.TG.Send(ans)
